0d02889639fdcda1e1a7f531215c3d51497593ea
Czirion <claudia.zirion@cinvestav.mx>
2022-06-21 18:36:18 -0500

    Inicio de repo con scripts/ refseq_210622/ quast/ y metadatos.csv

---
+++ scripts/download_and_metadata.sh
@@ -0,0 +1,51 @@
+## Download, exploration and metadate generation script
+
+#Usage:
+# Locate yourself in the global project folder where the scripts/download_explore.sh is located
+# bash scripts/download_and_metadata.sh <date(only numbers)>
+
+#Requisites:
+# - Have Quast installed
+# - Have R installed
+# - Put the quast_a_metadatos.R script in the scripts/ folder
+
+#Download Mycobacterium tuberculosis variant africanum in genebank and fasta format
+ncbi-genome-download -P -r 3 -F "fasta,genbank" --genera "Mycobacterium tuberculosis variant africanum" bacteria
+
+# Change foldername
+echo "Renaming refseq/"
+mv refseq/ refseq_"$1"
+
+#Decompress
+echo "Decompressing files"
+gunzip refseq_"$1"/bacteria/GCF_*/*.gz
+
+#Run Quast
+echo "Running Quast"
+quast -o quast/ --space-efficient refseq_"$1"/bacteria/GCF_*/*.fna
+
+#Run quast_to_metadata.tsv
+echo "Creating metadata table"
+Rscript scripts/quast_a_metadatos.R
+
+#Print the name of every genome
+#WARNING: Some genomes may have extra strings like ", complete genome" along the complete name
+#grep "DEFINITION" GCF_*/*.gbff | uniq
+
+
+
+
+
+#Print name of file with 
+#for folder in GCF*
+#	do
+#		filename=$(echo $folder/*.gbff)
+#		total_genes=$(grep "Genes (total)" $folder/*gbff | grep "Pseudo" -v | uniq | cut -d':' -f4)
+#		echo $filename $total_genes
+#	done | cut -d'/' -f2
+#	 Genes (total)                     :: 4,107
+#            CDSs (total)                      :: 4,056
+#           Genes (coding)                    :: 3,851
+#            CDSs (with protein)
+#
+#            Genes (total) :: 4,097
---
+++ scripts/quast_a_metadatos.R
@@ -0,0 +1,17 @@
+#Script to make a little matadate table from some of the Quast results 
+# with only Filename, number of contigs, genome length and GC%
+#Must be used from the folder that has the quast/ output folder 
+# and where the metadata.tsv will be created
+
+library(tidyverse)
+setwd(getwd())
+parametros <- read.delim("quast/report.tsv", sep = "\t",header = FALSE)
+parametros <- t(parametros) #transpose
+parametros <- as.data.frame(parametros) #return object to dataframe type because the transposition changed it
+parametros <- select(parametros, V1,V14, V16, V17)
+colnames(parametros) <- c("Filename", "Contigs", "GenomeLength", "GC")
+parametros <- parametros[-1,]
+parametros$Contigs <- as.numeric(parametros$Contigs)
+parametros$GenomeLength <- as.numeric(parametros$GenomeLength)
+parametros$GC <- as.numeric(parametros$GC)
+write.table(parametros, file='metadatos.tsv', quote=FALSE, sep='\t', row.names = FALSE)
