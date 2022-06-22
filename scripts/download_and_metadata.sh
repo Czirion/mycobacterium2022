7a77dc3dfcd9fcac45f97433e7facf51b615c973
Czirion <claudia.zirion@cinvestav.mx>
2022-06-22 13:19:01 -0500

    Script con mas argumentos y todo en la misma carpeta

--- scripts/download_and_metadata.sh
+++ scripts/download_and_metadata.sh
@@ -2,7 +2,12 @@
 
 #Usage:
 # Locate yourself in the global project folder where the scripts/download_explore.sh is located
-# bash scripts/download_and_metadata.sh <date(only numbers)>
+# bash scripts/download_and_metadata.sh arguments
+# Positional arguments:
+# 1: species abreviation for foldername
+# 2: date in only numbers
+# 3: formats to download (coma separated without spaces) surrounded by double ticks
+# 4: lineaje name for NCBI download surrounded by double ticks
 
 #Requisites:
 # - Have Quast installed
@@ -10,23 +15,30 @@
 # - Put the quast_a_metadatos.R script in the scripts/ folder
 
 #Download Mycobacterium tuberculosis variant africanum in genebank and fasta format
-ncbi-genome-download -P -r 3 -F "fasta,genbank" --genera "Mycobacterium tuberculosis variant africanum" bacteria
+
+
+ncbi-genome-download -P -r 3 -F "$3" --genera "$4" bacteria
 
 # Change foldername
-echo "Renaming refseq/"
-mv refseq/ refseq_"$1"
+echo "Renaming folders"
+mv refseq/ "$1"_"$2"/
+mv "$1"_"$2"/bacteria/ "$1"_"$2"/raw_data/
+
 
 #Decompress
 echo "Decompressing files"
-gunzip refseq_"$1"/bacteria/GCF_*/*.gz
+gunzip "$1"_"$2"/raw_data/GCF_*/*.gz
+
+cd "$1"_"$2"/
 
 #Create a table with all the needed parameters taken from the gbff
 #The DEFINITION field has the name of the organism (Genus species variand strain) but sometimes also includes extra information about the sequence, like ",complete genome"
+echo "Extracting parameters from data"
 echo -e "Assembly""\t"Definition"\t"GenesTotal"\t"CDSsTotal"\t"GenesCoding"\t""CDSsProtein" > gbk_parameters.tsv
 
-ls refseq_"$1"/bacteria/GCF_*/*.gbff | while read line;
+ls raw_data/GCF_*/*.gbff | while read line;
 do 
-assembly=$(echo $line | cut -d'/' -f3);
+assembly=$(echo $line | cut -d'/' -f2);
 taxonomy=$(grep "DEFINITION" $line | uniq | cut -d' ' -f1,2 --complement)
 total_genes=$(grep "Genes (total)" $line | grep "Pseudo" -v | uniq | rev | cut -d' ' -f1 | rev);
 total_cds=$(grep "CDSs (total)" $line | uniq | rev | cut -d' ' -f1 | rev );
@@ -35,13 +47,14 @@ protein_cds=$(grep "CDSs (with protein)" $line | uniq | rev | cut -d' ' -f1 | re
 echo -e $assembly"\t"$taxonomy"\t"$total_genes"\t"$total_cds"\t"$coding_genes"\t"$protein_cds >> gbk_parameters.tsv;
 done
 
+
 #Run Quast
 echo "Running Quast"
-quast -o quast/ --space-efficient refseq_"$1"/bacteria/GCF_*/*.fna
+quast -o quast/ --space-efficient raw_data/GCF_*/*.fna
 
 #Run quast_to_metadata.tsv
 echo "Creating metadata table"
-Rscript scripts/crear_metadatos.R
+Rscript ../scripts/crear_metadatos.R
 
 #Remove gbk_parameters.tsv
 rm gbk_parameters.tsv
