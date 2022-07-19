c999f095dd1cba3ad7f98b5b32a4c23bdc0f7859
Czirion <claudia.zirion@cinvestav.mx>
2022-07-18 20:23:01 -0500

    update

--- reads_mtb_sra/limpieza_metadatos_sra.R
+++ reads_mtb_sra/limpieza_metadatos_sra.R
@@ -7,5 +7,4 @@ sraMetadata<- read.table("SraRunInfo.csv",
                                stringsAsFactors = TRUE, 
                                fill = TRUE,
                                quote = "")
-biosample_list <- select(sraMetadata, BioSample)
-write_tsv(biosample_list, "biosample_list.txt",na = "", col_names = FALSE) #Put the table in a file
+
