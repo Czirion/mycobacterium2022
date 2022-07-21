74f72a41caa811bd50f144683ba297a32483a760
Czirion <claudia.zirion@cinvestav.mx>
2022-07-21 09:10:51 -0500

    Empexar con tabla modificada

--- who/obtain_run_id.R
+++ who/obtain_run_id.R
@@ -1,9 +1,14 @@
 library(tidyverse)
 setwd("/home/claudia/Documentos/tec/mycobacterium2022/who/")
-MMC2_S1<- readxl::read_xlsx("mmc2.xlsx", sheet = "S1")
+MMC2_S1<- readxl::read_xlsx("mmc2_modificado.xlsx", sheet = "S1", col_types = "text")
+cols <- colnames(MMC2_S1)
+MMC2_S1[cols] <- lapply(MMC2_S1[cols], factor)
+
+cols <- c("","b","c","d")
 enaRunList <- MMC2_S1[,6] %>%
   na.omit()%>%
   as.data.frame()
+
 
 cols <- c("a","b","c","d")
 enaRunList <- enaRunList %>%
