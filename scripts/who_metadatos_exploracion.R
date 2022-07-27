9d855c3e2d484b50c98b29c17333d03d684f5cc6
Czirion <claudia.zirion@cinvestav.mx>
2022-07-26 19:52:37 -0500

    cargar todas las tablas de metadatos con funcion y for

--- scripts/who_metadatos_exploracion.R
+++ scripts/who_metadatos_exploracion.R
@@ -1,20 +1,22 @@
 library(tidyverse)
+
 setwd("/home/claudia/Documentos/tec/mycobacterium2022/who/metadata_fragm_tables/")
 
-#### metadata_SRA_todos ####
-metadata_SRA_todos <- read.table("metadata_SRA_todos.tsv",
-                                 sep = "\t", 
-                                 header = TRUE, 
-                                 na.strings=c("","NA"), 
-                                 stringsAsFactors = TRUE, 
-                                 fill = TRUE,
-                                 quote = "")
+#### Load all tables into dataframes ####
+cargar_tablas <- function(file){
+  tabla <- read.table(file,
+             sep = "\t", 
+             header = TRUE, 
+             na.strings=c("","NA"), 
+             stringsAsFactors = TRUE, 
+             fill = TRUE,
+             quote = "")
+  return(tabla)
+}
+for (file in 1:15) {
+   temp <- list.files(".")[file]
+   assign(file_path_sans_ext(temp),cargar_tablas(temp))
+   rm(file,temp)
+   }
 
-#### metadata_SRA_todos ####
-metadata_biosample_runA <- read.table("metadata_biosample_runA.tsv",
-                                 sep = "\t", 
-                                 header = TRUE, 
-                                 na.strings=c("","NA"), 
-                                 stringsAsFactors = TRUE, 
-                                 fill = TRUE,
-                                 quote = "")
+#### 
